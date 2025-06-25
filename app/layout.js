c53bb3246d16e023c46f343d2bae41a7e5318911
Anchovy0123 <suwannasarn0123@gmail.com>
2025-06-25 13:28:57 +0700

    test2

--- app/layout.js
+++ app/layout.js
@@ -1,18 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Geist, Geist_Mono } from "next/font/google";
-// import "./globals.css";
 import Navigation from "./components/nav";
 import Footer from './components/footer';
 import { Prompt } from 'next/font/google';
-
+// import "./globals.css";
 
 const prompt = Prompt({
-  subsets: ['thai', 'latin'], // รองรับภาษาไทย
+  subsets: ['thai', 'latin'],
   weight: ['300', '400', '500', '600', '700'],
   display: 'swap',
 });
-
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,10 +29,13 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={prompt.className}>
+    <html lang="th">
+      <body className={`${prompt.className} ${geistSans.variable} ${geistMono.variable} d-flex flex-column min-vh-100`}>
         <Navigation />
-        {children}
+        <main className="flex-grow-1">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
