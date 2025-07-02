590153993998989db41f41a6fbeebaeb89714111
Anchovy0123 <suwannasarn0123@gmail.com>
2025-07-02 12:12:16 +0700

    WOW1

--- app/components/card.js
+++ app/components/card.js
@@ -1,40 +1,75 @@
 'use client';
 import { useEffect } from 'react';
 import Image from 'next/image';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function Carousel() {
+export default function CardList() {
   useEffect(() => {
     import('bootstrap/dist/js/bootstrap.bundle.min.js');
   }, []);
 
   return (
-<div>
-  <div className="card" style={{width: '18rem'}}>
-    <img src="/images/sliders/Exusiai.webp" className="card-img-top" alt="..." />
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-  <div className="card" style={{width: '18rem'}}>
-    <img src="/images/sliders/images (4).jpg" className="card-img-top" alt="..." />
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-  <div className="card" style={{width: '18rem'}}>
-    <img src="/images/sliders/download.jpg" className="card-img-top" alt="..." />
-    <div className="card-body">
-      <h5 className="card-title">Card title</h5>
-      <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
-      <a href="#" className="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
-</div>
+    <div className="container my-4">
+      <div className="row g-3">
+        <div className="col-md-4">
+          <div className="card">
+            <Image
+              src="/images/sliders/Exusiai.webp"
+              className="card-img-top"
+              alt="Exusiai"
+              width={288}
+              height={400}
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up the bulk of the card’s content.
+              </p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
 
+        <div className="col-md-4">
+          <div className="card">
+            <Image
+              src="/images/sliders/Exusiai_Skin_3.webp"
+              className="card-img-top"
+              alt="Image 4"
+              width={288}
+              height={400}
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up the bulk of the card’s content.
+              </p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-4">
+          <div className="card">
+            <Image
+              src="/images/sliders/Exusiai_Elite_2.webp"
+              className="card-img-top"
+              alt="Download"
+              width={288}
+              height={400}
+            />
+            <div className="card-body">
+              <h5 className="card-title">Card title</h5>
+              <p className="card-text">
+                Some quick example text to build on the card title and make up the bulk of the card’s content.
+              </p>
+              <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
+
 
--- app/page.js
+++ app/page.js
@@ -2,16 +2,15 @@
 import { useEffect } from 'react';
 import Image from "next/image";
 import Carousel from "./components/carousel";
-
+import Card from "./components/Card";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-      
+      <Carousel />
       <h1>010 โชติกร สุวรรณสาร</h1>
-      <Carousel/>
-      </main>
-    
+      <Card />  
+      </main>   
     </div>
   );
 }
