25a7fb100db7f77014278ed05bccfa6ff5a3df9a
Anchovy0123 <suwannasarn0123@gmail.com>
2025-07-02 12:47:41 +0700

    d

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 import { useEffect } from 'react';
 import Image from "next/image";
 import Carousel from "./components/carousel";
-import Card from "./components/Card";
+import Card from "./components/Card"
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
